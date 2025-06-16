52da5b73d62f5555d2d1bea0ff72e8bed2fd339e
sholvoir <sovar.he@gmail.com>
2025-06-16 01:48:18 -0400

    0.11.1

--- vite.worker.config.ts
+++ vite.worker.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [preact(), UnoCSS()],
   build: {
     target: 'esnext',
     outDir: '../sholvoir.github.io/memword',
-    emptyOutDir: true,
     assetsDir: '',
     rollupOptions: {
-      input: ['index.html', 'admin.html']
+      input: 'lib/worker.ts',
+      output: {
+        entryFileNames: '[name].js'
+      }
     }
   }
 })
