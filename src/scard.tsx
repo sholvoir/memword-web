25cdbd1710d59aa218dc20b68f0143fc38cf2a43
sholvoir <sovar.he@gmail.com>
2025-07-11 13:54:20 -0400

    0.14.4

--- src/scard.tsx
+++ src/scard.tsx
@@ -1,7 +1,9 @@
 import { ICard } from "../../memword-server/lib/idict.ts";
 
 export default ({ card }: { card?: ICard }) =>
-    card?.def?.split('\n').map((t: string, i: number) => t.startsWith(' ') ?
-        <p key={i} class="text-lg">&ensp;&bull;{t}</p> :
-        <p key={i} class="text-xl font-bold">{t}</p>
-    )
+    card?.def?.split('\n').map((t: string, i: number) => {
+        if (t.startsWith(' ')) {
+            const [e, c] = t.split('|');
+            return <p key={i} class="text-lg">&ensp;-{e}<b class="text-xl">{c}</b></p>
+        } else return <p key={i} class="text-xl font-bold">{t}</p>
+    })
