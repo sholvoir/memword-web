adc7102240c19fc46c734793f1e5c0adcc2d2de3
sholvoir <sovar.he@gmail.com>
2025-08-12 17:54:39 -0400

    0.17.18

--- lib/server.ts
+++ lib/server.ts
@@ -30,13 +30,13 @@ export const deleteDict = (word: string) =>
 	);
 
 export const postTasks = (tasks: Array<ITask>) =>
-	fetch(`${API_URL}/api/task`, requestInit(tasks, "POST", authHead));
+	fetch(`${API_URL}/api/v1task`, requestInit(tasks, "POST", authHead));
 
 export const deleteTask = (words: Array<string>) =>
-	fetch(`${API_URL}/api/task`, requestInit(words, "DELETE", authHead));
+	fetch(`${API_URL}/api/v1task`, requestInit(words, "DELETE", authHead));
 
 export const postIssue = (issue: string) =>
-	fetch(`${API_URL}/api/issue`, requestInit({ issue }, "POST", authHead));
+	fetch(`${API_URL}/api/v1issue`, requestInit({ issue }, "POST", authHead));
 
 export const getBooks = () => getJson<Array<IBook>>(`${API_URL}/pub/book`);
 
@@ -44,21 +44,21 @@ export const getBook = (bid: string) => fetch(`${API_URL}/pub/book/${bid}`);
 
 export const postBook = (name: string, words: string, disc?: string) =>
 	getRes(
-		`${API_URL}/api/book`,
+		`${API_URL}/api/v1book`,
 		{ name, disc },
 		{ body: words, method: "POST", headers: authHead },
 	);
 
 export const putBook = (name: string, words: string, disc?: string) =>
 	getRes(
-		`${API_URL}/api/book`,
+		`${API_URL}/api/v1book`,
 		{ name, disc },
 		{ body: words, method: "PUT", headers: authHead },
 	);
 
 export const deleteBook = (name: string) =>
 	getRes(
-		`${API_URL}/api/wordlist`,
+		`${API_URL}/api/v1wordlist`,
 		{ name },
 		{ method: "DELETE", headers: authHead },
 	);
@@ -74,7 +74,7 @@ export const getSound = (url: string) =>
 	getRes(`${API_URL}/pub/sound`, { q: url }, { cache: "force-cache" });
 
 export const postSetting = (setting: ISetting) =>
-	fetch(`${API_URL}/api/setting`, requestInit(setting, "POST", authHead));
+	fetch(`${API_URL}/api/v1setting`, requestInit(setting, "POST", authHead));
 
 export const getIssues = () =>
 	getJson<Array<IIssue>>(`${API_URL}/admin/issue`, undefined, {
