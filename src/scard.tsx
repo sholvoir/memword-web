64dee0b0b44f3519d82810dd3f2936858de57ef6
sholvoir <sovar.he@gmail.com>
2025-09-12 16:09:37 -0400

    0.19.7

--- src/scard.tsx
+++ src/scard.tsx
@@ -1,10 +1,12 @@
 import type { IEntry } from "@sholvoir/memword-common/idict";
 import { For, Show } from "solid-js";
+import * as mem from "../lib/mem.ts";
+
 
 export default (props: { entry: IEntry }) => (
 	<For each={Object.entries(props.entry.meanings!)}>
 		{([pos, means]) => (
-			<>
+			<Show when={mem.setting.trans || pos !== 'ecdict'}>
 				<Show when={pos}>
 					<p class="text-xl font-bold text-[var(--accent-color)]">{pos}</p>
 				</Show>
@@ -15,16 +17,16 @@ export default (props: { entry: IEntry }) => (
 							<Show when={item.def}>
 								<span class="text-lg">{item.def}</span>
 							</Show>
-							<Show when={item.def && item.trans}>
+							<Show when={mem.setting.trans && item.def && item.trans}>
 								<span>&ensp;</span>
 							</Show>
-							<Show when={item.trans}>
+							<Show when={mem.setting.trans && item.trans}>
 								<span class="text-xl font-bold">{item.trans}</span>
 							</Show>
 						</p>
 					)}
 				</For>
-			</>
+			</Show>
 		)}
 	</For>
 );
