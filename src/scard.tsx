9d81644f258405c8fdcbc635ff0bce49d602a2bd
sholvoir <sovar.he@gmail.com>
2025-09-16 15:50:17 -0400

    0.19.8

--- src/scard.tsx
+++ src/scard.tsx
@@ -1,12 +1,10 @@
 import type { IEntry } from "@sholvoir/memword-common/idict";
 import { For, Show } from "solid-js";
-import * as mem from "../lib/mem.ts";
 
-
-export default (props: { entry: IEntry }) => (
+export default (props: { entry: IEntry, trans?: boolean }) => (
 	<For each={Object.entries(props.entry.meanings!)}>
 		{([pos, means]) => (
-			<Show when={mem.setting.trans || pos !== 'ecdict'}>
+			<Show when={props.trans || pos !== 'ecdict'}>
 				<Show when={pos}>
 					<p class="text-xl font-bold text-[var(--accent-color)]">{pos}</p>
 				</Show>
@@ -17,10 +15,10 @@ export default (props: { entry: IEntry }) => (
 							<Show when={item.def}>
 								<span class="text-lg">{item.def}</span>
 							</Show>
-							<Show when={mem.setting.trans && item.def && item.trans}>
+							<Show when={props.trans && item.def && item.trans}>
 								<span>&ensp;</span>
 							</Show>
-							<Show when={mem.setting.trans && item.trans}>
+							<Show when={props.trans && item.trans}>
 								<span class="text-xl font-bold">{item.trans}</span>
 							</Show>
 						</p>
--- src/study.tsx
+++ src/study.tsx
@@ -16,252 +16,264 @@ import Dialog from "./dialog.tsx";
 import Scard from "./scard.tsx";
 
 export default () => {
-	const finish = async () => {
-		app.go(app.sprint() < 0 ? "#search" : undefined);
-		await app.totalStats();
-		await mem.syncTasks();
-		app.totalStats();
-	};
-	const [cindex, setCIndex] = createSignal(0);
-	const touchPos = { startY: 0, endY: 0, offset: 0, canUp: false, canDown: false };
+   const finish = async () => {
+      app.go(app.sprint() < 0 ? "#search" : undefined);
+      await app.totalStats();
+      await mem.syncTasks();
+      app.totalStats();
+   };
+   const [cindex, setCIndex] = createSignal(0);
+   const touchPos = {
+      startY: 0,
+      endY: 0,
+      offset: 0,
+      canUp: false,
+      canDown: false,
+   };
 
-	const [myBooks, setMyBooks] = createSignal<Array<IBook>>([]);
-	const showAddToBookMenu = createSignal(false);
-	let player!: HTMLAudioElement;
-	const handleIKnown = async (level?: number) => {
-		if (app.citem()) await idb.studied(app.citem()!.word, level);
-	};
-	const studyNext = async () => {
-		if (app.sprint() < 0) return finish();
-		app.setSprint((s) => s + 1);
-		app.setCItem(undefined);
-		app.setPhaseAnswer(false);
-		const item = await mem.getEpisode(app.bid());
-		if (!item) return finish();
-		app.setCItem(item);
-		setCIndex(0);
-	};
-	const handleRefresh = async () => {
-		app.showTips("Get Server Data...", false);
-		const item = await mem.updateDict(app.citem()!);
-		app.hideTips();
-		app.setCItem({ ...item });
-	};
-	const handleReportIssue = async () => {
-		app.showTips("Submiting...", false);
-		await mem.submitIssue(app.citem()!.word);
-		app.showTips("Submit Success!");
-	};
-	const handleDelete = async () => {
-		app.showTips(
-			(await mem.deleteItem(app.citem()!.word)) ? "删除成功" : "删除失败",
-		);
-		await studyNext();
-	};
-	const handleKeyPress = (e: KeyboardEvent & DivTargeted) => {
-		e.stopPropagation();
-		if (e.ctrlKey || e.altKey) return;
-		switch (e.key) {
-			case " ":
-				handleClick(e as any);
-				break;
-			case "N":
-			case "X":
-			case "n":
-			case "x":
-				if (app.isPhaseAnswer()) handleIKnown().then(studyNext);
-				break;
-			case "M":
-			case "Z":
-			case "m":
-			case "z":
-				if (app.isPhaseAnswer()) handleIKnown(0).then(studyNext);
-				break;
-		}
-	};
-	const continueMove = async (div: HTMLDivElement, x: number) => {
-		div.style.top = `${touchPos.offset += x}px`;
-		if (Math.abs(touchPos.offset) < globalThis.innerHeight) {
-			await wait(30);
-			await continueMove(div, x);
-		}
-	};
-	const handleTouchStart = (e: TouchEvent & DivTargeted) => {
-		if (!app.isPhaseAnswer()) return;
-		const div = e.currentTarget;
-		touchPos.endY = touchPos.startY = e.touches[0].clientY;
-		touchPos.offset = 0;
-		touchPos.canDown = e.currentTarget.scrollTop <= 3;
-		touchPos.canUp = div.scrollHeight - div.clientHeight - div.scrollTop <= 3;;
-	};
-	const handleTouchMove = (e: TouchEvent & DivTargeted) => {
-		if (!app.isPhaseAnswer()) return;
-		touchPos.endY = e.touches[0].clientY;
-		const diff = touchPos.endY - touchPos.startY;
-		if ((diff < 0) && (touchPos.canUp) || (diff > 0) && (touchPos.canDown)) {
-			e.currentTarget.style.top = `${touchPos.offset=diff}px`;
-			e.stopPropagation();
-			e.preventDefault();
-		}
-	};
-	const handleTouchCancel = (e: TouchEvent & DivTargeted) => {
-		if (!app.isPhaseAnswer()) return;
-		e.currentTarget.style.top = `${touchPos.offset = 0}`;
-	};
-	const handleTouchEnd = async (e: TouchEvent & DivTargeted) => {
-		const div = e.currentTarget;
-		if (Math.abs(touchPos.offset) >= globalThis.innerHeight / 6) {
-			const down = touchPos.offset > 0;
-			await handleIKnown(down ? 0 : undefined);
-			await continueMove(div, down ? 60 : -60);
-			await studyNext();
-		}
-		div.style.top = `${touchPos.offset = 0}`;
-	};
-	const handleClick = (e?: MouseEvent & DivTargeted) => {
-		e?.stopPropagation();
-		if (showAddToBookMenu[0]()) return showAddToBookMenu[1](false);
-		const cardsN = app.citem()?.entries?.length ?? 0;
-		//if (cardsN === 0) return;
-		if (!app.isPhaseAnswer()) {
-			app.setPhaseAnswer(true);
-			player.play();
-		} else if (cardsN === 1) player.play();
-		else if (cindex() < cardsN - 1) setCIndex((c) => c + 1);
-		else setCIndex(0);
-	};
-	const handleAddToBook = async (book: IBook) => {
-		showAddToBookMenu[1](false);
-		const word = app.citem()!.word;
-		const wordSet = (await mem.getBook(book.bid))?.content as Set<string>;
-		if (wordSet?.has(word)) return app.showTips("已包含");
-		const [_, bookName] = splitID(book.bid);
-		const [status] = await mem.uploadBook(bookName, word);
-		app.showTips(status === STATUS_CODE.OK ? "添加成功" : "添加失败");
-		wordSet?.add(word);
-		((await mem.getVocabulary()) as Set<string>).add(word);
-	};
-	createResource(async () => {
-		setMyBooks(
-			await idb.getBooks((book) => splitID(book.bid)[0] === app.user()),
-		);
-	});
-	return (
-		<Dialog
-			left={
-				<BButton
-					class="text-[150%] icon-[material-symbols--chevron-left] align-bottom"
-					onClick={finish}
-				/>
-			}
-			onKeyUp={handleKeyPress}
-			tabIndex={-1}
-			class="flex flex-col p-2 outline-none"
-			title={`学习${app.sprint() > 0 ? `(${app.sprint()})` : ""}`}
-		>
-			<Show when={app.citem()}>
-				<div class="relative flex gap-4 text-[150%] justify-between items-end">
-					<SButton
-						onClick={() => handleIKnown().then(studyNext)}
-						title="X/N"
-						class="iconify icon-[material-symbols--check-circle] text-green-500"
-						disabled={!app.isPhaseAnswer()}
-					/>
-					<SButton
-						onClick={() => handleIKnown(0).then(studyNext)}
-						title="Z/M"
-						class="iconify icon-[mdi--cross-circle] text-fuchsia-500"
-						disabled={!app.isPhaseAnswer()}
-					/>
-					<SButton
-						onClick={() => handleIKnown(13).then(studyNext)}
-						class="iconify icon-[material-symbols--family-star] text-yellow-500"
-						disabled={!app.isPhaseAnswer()}
-					/>
-					<SButton
-						onClick={handleDelete}
-						class="iconify icon-[material-symbols--delete-outline] text-orange-500"
-						disabled={!app.isPhaseAnswer()}
-					/>
-					<SButton
-						onClick={() => player.play()}
-						class="iconify icon-[material-symbols--volume-up] text-blue-500"
-					/>
-					<SButton
-						onClick={handleReportIssue}
-						class="iconify icon-[material-symbols--error] text-red-500"
-						disabled={!app.isPhaseAnswer()}
-					/>
-					<SButton
-						onClick={handleRefresh}
-						class="iconify icon-[material-symbols--refresh] text-purple-500"
-						disabled={!app.isPhaseAnswer()}
-					/>
-					<SButton
-						onClick={() => showAddToBookMenu[1]((s) => !s)}
-						class="iconify icon-[material-symbols--dictionary-outline] text-cyan-500"
-						disabled={!app.isPhaseAnswer()}
-					></SButton>
-					<div class="text-lg">{app.citem()?.level}</div>
-					<Show when={showAddToBookMenu[0]()}>
-						<div class="menu absolute top-[100%] right-[36px] text-lg text-right bg-[var(--bg-body)] z-1">
-							<For each={myBooks()}>
-								{(wl) => (
-									<>
-										<div />
-										<menu onClick={() => handleAddToBook(wl)}>
-											{wl.disc ?? wl.bid}
-										</menu>
-									</>
-								)}
-							</For>
-							{myBooks().length && <div />}
-						</div>
-					</Show>
-				</div>
-				<div
-					class="relative grow h-0 pb-4 flex flex-col overflow-y-auto"
-					on:click={handleClick}
-					on:touchstart={handleTouchStart}
-					on:touchmove={handleTouchMove}
-					on:touchend={handleTouchEnd}
-					on:touchcancel={handleTouchCancel}
-				>
-					<div class="py-2 flex gap-2 flex-wrap justify-between">
-						<div class="text-4xl font-bold">{app.citem()?.word}</div>
-						{app.isPhaseAnswer() && (
-							<div class="text-2xl flex items-center">
-								{app.citem()?.entries?.[cindex()].phonetic}
-							</div>
-						)}
-					</div>
-					<Show when={app.isPhaseAnswer()}>
-						<Show
-							when={(app.citem()?.entries?.length ?? 0) > 1}
-							fallback={
-								<div class="grow">
-									<Scard entry={app.citem()?.entries?.[0]!} />
-								</div>
-							}
-						>
-							<Tab class="bg-[var(--bg-tab)]" cindex={[cindex, setCIndex]}>
-								<For each={app.citem()?.entries}>
-									{(card) => (
-										<div class="grow">
-											<Scard entry={card} />
-										</div>
-									)}
-								</For>
-							</Tab>
-						</Show>
-					</Show>
-					<audio
-						ref={player}
-						autoplay
-						src={app.citem()?.entries?.at(cindex())?.sound ?? ""}
-					/>
-				</div>
-			</Show>
-		</Dialog>
-	);
+   const [myBooks, setMyBooks] = createSignal<Array<IBook>>([]);
+   const showAddToBookMenu = createSignal(false);
+   let player!: HTMLAudioElement;
+   const handleIKnown = async (level?: number) => {
+      if (app.citem()) await idb.studied(app.citem()!.word, level);
+   };
+   const studyNext = async () => {
+      if (app.sprint() < 0) return finish();
+      app.setSprint((s) => s + 1);
+      app.setCItem(undefined);
+      app.setPhaseAnswer(false);
+      const item = await mem.getEpisode(app.bid());
+      if (!item) return finish();
+      app.setCItem(item);
+      setCIndex(0);
+   };
+   const handleRefresh = async () => {
+      app.showTips("Get Server Data...", false);
+      const item = await mem.updateDict(app.citem()!);
+      app.hideTips();
+      app.setCItem({ ...item });
+   };
+   const handleReportIssue = async () => {
+      app.showTips("Submiting...", false);
+      await mem.submitIssue(app.citem()!.word);
+      app.showTips("Submit Success!");
+   };
+   const handleDelete = async () => {
+      app.showTips(
+         (await mem.deleteItem(app.citem()!.word)) ? "删除成功" : "删除失败",
+      );
+      await studyNext();
+   };
+   const handleKeyPress = (e: KeyboardEvent & DivTargeted) => {
+      e.stopPropagation();
+      if (e.ctrlKey || e.altKey) return;
+      switch (e.key) {
+         case " ":
+            handleClick(e as any);
+            break;
+         case "N":
+         case "X":
+         case "n":
+         case "x":
+            if (app.isPhaseAnswer()) handleIKnown().then(studyNext);
+            break;
+         case "M":
+         case "Z":
+         case "m":
+         case "z":
+            if (app.isPhaseAnswer()) handleIKnown(0).then(studyNext);
+            break;
+      }
+   };
+   const continueMove = async (div: HTMLDivElement, x: number) => {
+      div.style.top = `${(touchPos.offset += x)}px`;
+      if (Math.abs(touchPos.offset) < globalThis.innerHeight) {
+         await wait(30);
+         await continueMove(div, x);
+      }
+   };
+   const handleTouchStart = (e: TouchEvent & DivTargeted) => {
+      if (!app.isPhaseAnswer()) return;
+      const div = e.currentTarget;
+      touchPos.endY = touchPos.startY = e.touches[0].clientY;
+      touchPos.offset = 0;
+      touchPos.canDown = e.currentTarget.scrollTop <= 3;
+      touchPos.canUp = div.scrollHeight - div.clientHeight - div.scrollTop <= 3;
+   };
+   const handleTouchMove = (e: TouchEvent & DivTargeted) => {
+      if (!app.isPhaseAnswer()) return;
+      touchPos.endY = e.touches[0].clientY;
+      const diff = touchPos.endY - touchPos.startY;
+      if ((diff < 0 && touchPos.canUp) || (diff > 0 && touchPos.canDown)) {
+         e.currentTarget.style.top = `${(touchPos.offset = diff)}px`;
+         e.stopPropagation();
+         e.preventDefault();
+      }
+   };
+   const handleTouchCancel = (e: TouchEvent & DivTargeted) => {
+      if (!app.isPhaseAnswer()) return;
+      e.currentTarget.style.top = `${(touchPos.offset = 0)}`;
+   };
+   const handleTouchEnd = async (e: TouchEvent & DivTargeted) => {
+      const div = e.currentTarget;
+      if (Math.abs(touchPos.offset) >= globalThis.innerHeight / 6) {
+         const down = touchPos.offset > 0;
+         await handleIKnown(down ? 0 : undefined);
+         await continueMove(div, down ? 60 : -60);
+         await studyNext();
+      }
+      div.style.top = `${(touchPos.offset = 0)}`;
+   };
+   const handleClick = (e?: MouseEvent & DivTargeted) => {
+      e?.stopPropagation();
+      if (showAddToBookMenu[0]()) return showAddToBookMenu[1](false);
+      const cardsN = app.citem()?.entries?.length ?? 0;
+      //if (cardsN === 0) return;
+      if (!app.isPhaseAnswer()) {
+         app.setPhaseAnswer(true);
+         player.play();
+      } else if (cardsN === 1) player.play();
+      else if (cindex() < cardsN - 1) setCIndex((c) => c + 1);
+      else setCIndex(0);
+   };
+   const handleAddToBook = async (book: IBook) => {
+      showAddToBookMenu[1](false);
+      const word = app.citem()!.word;
+      const wordSet = (await mem.getBook(book.bid))?.content as Set<string>;
+      if (wordSet?.has(word)) return app.showTips("已包含");
+      const [_, bookName] = splitID(book.bid);
+      const [status] = await mem.uploadBook(bookName, word);
+      app.showTips(status === STATUS_CODE.OK ? "添加成功" : "添加失败");
+      wordSet?.add(word);
+      ((await mem.getVocabulary()) as Set<string>).add(word);
+   };
+   createResource(async () => {
+      setMyBooks(
+         await idb.getBooks((book) => splitID(book.bid)[0] === app.user()),
+      );
+   });
+   return (
+      <Dialog
+         left={
+            <BButton
+               class="text-[150%] icon-[material-symbols--chevron-left] align-bottom"
+               onClick={finish}
+            />
+         }
+         onKeyUp={handleKeyPress}
+         tabIndex={-1}
+         class="flex flex-col p-2 outline-none"
+         title={`学习${app.sprint() > 0 ? `(${app.sprint()})` : ""}`}
+      >
+         <Show when={app.citem()}>
+            <div class="relative flex gap-4 text-[150%] justify-between items-end">
+               <SButton
+                  onClick={() => handleIKnown().then(studyNext)}
+                  title="X/N"
+                  class="iconify icon-[material-symbols--check-circle] text-green-500"
+                  disabled={!app.isPhaseAnswer()}
+               />
+               <SButton
+                  onClick={() => handleIKnown(0).then(studyNext)}
+                  title="Z/M"
+                  class="iconify icon-[mdi--cross-circle] text-fuchsia-500"
+                  disabled={!app.isPhaseAnswer()}
+               />
+               <SButton
+                  onClick={() => handleIKnown(13).then(studyNext)}
+                  class="iconify icon-[material-symbols--family-star] text-yellow-500"
+                  disabled={!app.isPhaseAnswer()}
+               />
+               <SButton
+                  onClick={handleDelete}
+                  class="iconify icon-[material-symbols--delete-outline] text-orange-500"
+                  disabled={!app.isPhaseAnswer()}
+               />
+               <SButton
+                  onClick={() => player.play()}
+                  class="iconify icon-[material-symbols--volume-up] text-blue-500"
+               />
+               <SButton
+                  onClick={handleReportIssue}
+                  class="iconify icon-[material-symbols--error] text-red-500"
+                  disabled={!app.isPhaseAnswer()}
+               />
+               <SButton
+                  onClick={handleRefresh}
+                  class="iconify icon-[material-symbols--refresh] text-purple-500"
+                  disabled={!app.isPhaseAnswer()}
+               />
+               <SButton
+                  onClick={() => showAddToBookMenu[1]((s) => !s)}
+                  class="iconify icon-[material-symbols--dictionary-outline] text-cyan-500"
+                  disabled={!app.isPhaseAnswer()}
+               ></SButton>
+               <div class="text-lg">{app.citem()?.level}</div>
+               <Show when={showAddToBookMenu[0]()}>
+                  <div class="menu absolute top-[100%] right-[36px] text-lg text-right bg-[var(--bg-body)] z-1">
+                     <For each={myBooks()}>
+                        {(wl) => (
+                           <>
+                              <div />
+                              <menu onClick={() => handleAddToBook(wl)}>
+                                 {wl.disc ?? wl.bid}
+                              </menu>
+                           </>
+                        )}
+                     </For>
+                     {myBooks().length && <div />}
+                  </div>
+               </Show>
+            </div>
+            <div
+               class="relative grow h-0 pb-4 flex flex-col overflow-y-auto"
+               on:click={handleClick}
+               on:touchstart={handleTouchStart}
+               on:touchmove={handleTouchMove}
+               on:touchend={handleTouchEnd}
+               on:touchcancel={handleTouchCancel}
+            >
+               <div class="py-2 flex gap-2 flex-wrap justify-between">
+                  <div class="text-4xl font-bold">{app.citem()?.word}</div>
+                  {app.isPhaseAnswer() && (
+                     <div class="text-2xl flex items-center">
+                        {app.citem()?.entries?.[cindex()].phonetic}
+                     </div>
+                  )}
+               </div>
+               <Show when={app.isPhaseAnswer()}>
+                  <Show
+                     when={(app.citem()?.entries?.length ?? 0) > 1}
+                     fallback={
+                        <div class="grow">
+                           <Scard
+                              entry={app.citem()?.entries?.[0]!}
+                              trans={app.sprint() < 0}
+                           />
+                        </div>
+                     }
+                  >
+                     <Tab
+                        class="bg-[var(--bg-tab)]"
+                        cindex={[cindex, setCIndex]}
+                     >
+                        <For each={app.citem()?.entries}>
+                           {(card) => (
+                              <div class="grow">
+                                 <Scard entry={card} trans={app.sprint() < 0} />
+                              </div>
+                           )}
+                        </For>
+                     </Tab>
+                  </Show>
+               </Show>
+               <audio
+                  ref={player}
+                  autoplay
+                  src={app.citem()?.entries?.at(cindex())?.sound ?? ""}
+               />
+            </div>
+         </Show>
+      </Dialog>
+   );
 };
